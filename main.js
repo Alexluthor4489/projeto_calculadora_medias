ffdf931185092cb6e8b2ed974b2aee163e3a5cf5
Alex Magalhaes Santos <alexmagalhaes4489@gmail.com>
2024-07-23 07:42:46 -0300

    fix limitando as casas decimais

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ function adicionaLinha() {
 
     inputNomeAtividade.value = ''
     inputNotaAtividade.value = ''
-    
+
 }
 function atualizaTabela() {
     const corpoTabela = document.querySelector('tbody')
@@ -51,7 +51,7 @@ function atualizaTabela() {
 function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal()
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2)
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado
 
         
